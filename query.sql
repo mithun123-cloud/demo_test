7516a3d5139f10be14d706aa8545b3d666737976
mithun123-cloud <63037398+mithun123-cloud@users.noreply.github.com>
2020-09-19 12:37:34 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,17 @@
+create table company3( 
+id int(10) not null auto_increment, 
+name varchar(50) not null, 
+contactNumber varchar(50) not null, 
+eMail varchar(50) not null, 
+address varchar(60) not null,
+uan varchar(30) not null,
+primary key (id)
+); 
+
+
+
+
+create table login11(
+username varchar(30) not null,
+ password varchar(30) not null
+ );
